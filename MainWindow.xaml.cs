289100f6c3077c1163f1b46598eda274a4f21e73
Murilo Abreu <murilo_abr@yahoo.com.br>
2026-08-19 10:41:39 -0300

    fix(ui): implement dynamic application icon loading from disk

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using CoreSearch.ViewModels;
 
 namespace CoreSearch;
@@ -9,6 +12,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SetWindowIcon();
+    }
+
+    private void SetWindowIcon()
+    {
+        try
+        {
+            string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.ico");
+            if (File.Exists(iconPath))
+            {
+                Icon = BitmapFrame.Create(new Uri(iconPath, UriKind.Absolute));
+            }
+        }
+        catch
+        {
+            // Ignora falha de carregamento de ícone se o arquivo estiver corrompido ou inacessível
+        }
     }
 
     private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
